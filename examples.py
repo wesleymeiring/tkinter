a78c2e65857a5d12fd8b5dd296ecd33f4dbf70f0
Wesley Meiring <wes.meiring@gmail.com>
2021-03-11 13:08:33 +0100

    Changed name of project

---
+++ examples.py
@@ -0,0 +1,8 @@
+imelda = 'More Mayhem', 'Imelda May', 2011, [
+    (1, 'Pulling the Rug'),
+    (2, 'Psycho'),
+    (3, 'Mayhem'),
+    (4, 'Kentish Town Waltz')]
+
+imelda[3].append(5, 'All For You')
+print(imelda[3])
